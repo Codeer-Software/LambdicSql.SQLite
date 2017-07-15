d0dedf282da15cd26d1c1277bfba53ec73d6b7b7
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-07-15 14:38:27 +0900

    separate .net 3.5 and 4.6.

---
+++ Project/LambdicSql.SQLite.NetFramework.4.6/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LambdicSql.SQLite.NetFramework.4.6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LambdicSql.SQLite.NetFramework.4.6")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("cca5be93-ecde-4fd1-9e7e-dd50dbde1a3a")]
+
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
